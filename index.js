3d05efe1fb467a40e2d4bc934c7c2bcbe8272a0b
Marak <marak.squires@gmail.com>
2017-10-12 18:33:58 -0400

    [api] Added ability to generate schema from json   * Useful for quickly generating forms   * Has basic type inference based on data

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ function generate (options, callback) {
   view.create({ path: __dirname + '/view', input: "html"}, function (err, view) {
     var str = '', form;
     form = view.form[options.type] || view.form['generic'];
+    // determine if no schema has been passed in, but data has
+    // if so, create auto-schema as best we can using text as the default values
+    if (typeof options.schema === 'undefined' && typeof options.data === 'object') {
+      options.schema = jsonToSchema(options.data);
+    }
     form.present(options, function(err, res){
       if(err) {
         throw err;
@@ -19,5 +24,26 @@ function generate (options, callback) {
     });
   });
 };
+
+// converts an abritrary JSON object into an mschema based on some intelligent type checking
+function jsonToSchema (data) {
+  var schema = {};
+  for (var p in data) {
+    if (typeof data[p] === 'function') {
+      // ignore function types
+      continue;
+    }
+    // default to string types
+    schema[p] = {
+      type: 'string',
+      default: data[p]
+    }
+    // ensure object types are set when detected
+    if (typeof data[p] === 'object') {
+      schema[p].type = 'object';
+    }
+  }
+  return schema;
+}
 
 module['exports'] = forms;
